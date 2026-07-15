3335d1251b08d5d142d9784de03d8bcfe8d298eb
Pavel Evstigneev <pavel.evst@gmail.com>
2026-07-15 23:40:10 +0400

    cleanup

--- lib/rack_resize/rails_autoload.rb
+++ lib/rack_resize/rails_autoload.rb
@@ -1,18 +1,9 @@
-require 'rack_resize'
-
-# Rails.application.config.to_prepare do
-#   require 'rack_resize'
-#   Rails.application.config.middleware.insert_before 0, RackResize::RackApp
-# end
-
-
+require "rack_resize"
 require "rails"
 
 module MyGem
   class Railtie < Rails::Railtie
     initializer "rack_resize.auto_register_itself" do |app|
-      # Insert your middleware into the stack
-      # app.config.middleware.use MyGem::MyMiddleware
       app.config.middleware.insert_before 0, RackResize::RackApp
     end
   end
